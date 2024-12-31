0b9640e4932b776a76b1351689184f73a004e6fe
manmen-mi <manmen.mi1375@gmail.com>
2024-12-31 15:36:17 +0900

    iter3???

--- solutions/18_iterators/iterators3.rs
+++ solutions/18_iterators/iterators3.rs
@@ -8,8 +8,6 @@ enum DivisionError {
     NotDivisible,
 }
 
-// TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
-// Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
     if b == 0 {
         return Err(DivisionError::DivideByZero);
@@ -26,19 +24,20 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
     Ok(a / b)
 }
 
-// TODO: Add the correct return type and complete the function body.
-// Desired output: `Ok([1, 11, 1426, 3])`
 fn result_with_list() -> Result<Vec<i64>, DivisionError> {
+    //                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // Collects to the expected return type. Returns the first error in the
+    // division results (if one exists).
     division_results.collect()
 }
 
-// TODO: Add the correct return type and complete the function body.
-// Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
 fn list_of_results() -> Vec<Result<i64, DivisionError>> {
+    //               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // Collects to the expected return type.
     division_results.collect()
 }
 
